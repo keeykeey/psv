3ee5f512aec214c1c4f644d2b0933edae2211b0d
keeykeey <sh1n_fuj1to@icloud.com>
2022-11-05 22:13:28 +0900

    レイアウト改善、フレキシブル対応

--- app/frontend/src/GetEnv.tsx
+++ app/frontend/src/GetEnv.tsx
@@ -1,9 +1,29 @@
 interface Env {
-    api_url     :     string
+    api_url   :  string
+    os        :  string //"mac" | "win" | "android" | "linux",
+    browser   :  string //"chrome" | "safari" | "edge" | "firefox",
+    window_h  :  number
+    window_w  :  number
 }
 
+const info:string = window.navigator.userAgent;
+
+function getEnvOs() {
+    return 'mac'
+}
+const env_os = getEnvOs()
+
+function getEnvBrouser() {
+    return 'chrome'
+}
+const env_browser = getEnvBrouser()
+
 const env : Env = {
-    api_url : "https://localhost:8000"
+    api_url   : "https://APIURL:8000",
+    os        : env_os,
+    browser   : env_browser,
+    window_h  : window.innerHeight,
+    window_w  : window.innerWidth
 }
 
 export default env;
